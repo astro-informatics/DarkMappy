af9a79bf62a4b533fdd94495dee8841d41fa0447
CosmoMatt <matt.price@kagenova.com>
2021-12-20 16:03:16 +0000

    add optimus-primal temporary manual install in bash script

--- build_darkmappy.sh
+++ build_darkmappy.sh
@@ -10,6 +10,12 @@ pip install -q -r requirements/requirements-tests.txt
 echo -ne 'Building Dependencies... ################   (100%)\r'
 echo -ne '\n'
 
+# Install Optimus-Primal (TODO: update PyPi and add to core reqs)
+git clone git@github.com:astro-informatics/Optimus-Primal.git .optimus-primal
+cd .optimus-primal
+pip install .
+cd ..
+
 # Install specific converter for building tutorial documentation
 conda install pandoc=1.19.2.1 -y
 
